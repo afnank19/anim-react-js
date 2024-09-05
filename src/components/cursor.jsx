f7796c73c95a65e9250da16c8914cce99e5bd601
Afnan Khalid <afnank19@gmail.com>
2024-09-05 23:50:03 +0500

    finishing touches on landing page

--- src/components/cursor.jsx
+++ src/components/cursor.jsx
@@ -6,6 +6,7 @@ const Cursor = () => {
     const mouse = {
         x: useMotionValue(0),
         y: useMotionValue(0),
+        op: useMotionValue(1)
     }
 
     const smoothOptions = {damping: 20, stiffness: 200, mass: 0.5}
@@ -18,10 +19,14 @@ const Cursor = () => {
         const { clientX, clientY} = e;
         mouse.x.set(clientX - size / 2);
         mouse.y.set(clientY -size / 2);
+        mouse.op.set(1)
     }
 
     useEffect(()=> {
         window.addEventListener("mousemove", manageMouseMove)
+        window.addEventListener("touchmove", (e) => {
+            mouse.op.set(0)
+        })
 
         return () => {window.removeEventListener("mousemove", manageMouseMove)}
     })
@@ -29,7 +34,7 @@ const Cursor = () => {
 
 
   return (
-    <motion.div className='cursor' style={{left: smoothMouse.x, top: smoothMouse.y}}></motion.div>
+    <motion.div className='cursor' style={{left: smoothMouse.x, top: smoothMouse.y, opacity: mouse.op}}></motion.div>
   )
 }
 
